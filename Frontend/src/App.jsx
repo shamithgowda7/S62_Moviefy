ce4a67f7dd10eb08b0f49f02f0f4c6babe9d6ed0
jeevanhd <jeevan.hd@kalvium.community>
2025-01-23 09:55:15 +0530

    m9

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,20 @@
-import React from "react";
+import { useState } from 'react';
+import './App.css';
 
-export const App = () => {
-  return <div>App</div>;
-};
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Welcome to the ASAP Project</h1>
+        <p>
+          This project aims to create a platform where users can leave reviews on movies and discover new films within the same genre.
+        </p>
+        <p>
+          Stay tuned for more updates as we develop this exciting project!
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
