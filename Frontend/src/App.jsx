136bf2ac4fd146a1281af3635d4b6ef2f45aa0e3
Shamith <gshamith72@gmail.com>
2025-01-23 21:44:30 +0530

    changed

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,20 +1,8 @@
-import { useState } from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <>
-      <div>
-        <h1>Welcome to the ASAP Project</h1>
-        <p>
-          This project aims to create a platform where users can leave reviews on movies and discover new films within the same genre.
-        </p>
-        <p>
-          Stay tuned for more updates as we develop this exciting project!
-        </p>
-      </div>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
